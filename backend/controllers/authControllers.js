f1b94bb45f42ed49e1f9184797aa81fbca18e84a
Jatin-4291 <jatin.dua2003@gmail.com>
2024-03-08 02:50:54 +0530

    added api features and handleFactory

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,4 +1,4 @@
-import User from "../models/usermodels.js";
+import User from "../models/userModel.js";
 
 import { promisify } from "util";
 import jwt from "jsonwebtoken";
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
 
 import { signUp, login } from "../controllers/authControllers.js";
+import {
+  getAllUsers,
+  getOneUser,
+  deleteOneUser,
+} from "../controllers/userControllers.js";
 
+console.log(getAllUsers);
 const Router = express.Router(); // Change this line
 
 Router.post("/signup", signUp);
 Router.post("/login", login);
+Router.get("/getAllUsers", getAllUsers);
+Router.get("/getOneUser", getOneUser);
+Router.delete("/deleteOneUser", deleteOneUser);
 
 export default Router;
