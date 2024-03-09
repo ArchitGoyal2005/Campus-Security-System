761e7e62670d8b31b3f7f14e6e88244c2d74311a
Archit Goyal <architgoyal2005@gmail.com>
2024-03-10 01:52:06 +0530

    added transit controler and routes

--- backend/models/transitModel.js
+++ backend/models/transitModel.js
@@ -1,14 +1,37 @@
 import mongoose from "mongoose";
 
-const transitSchema = mongoose.Schema({
-  entryGate: {
-    type: Number,
+const transitSchema = mongoose.Schema(
+  {
+    entryGate: {
+      type: Number,
+    },
+    exitGate: {
+      type: Number,
+    },
+    entryTime: {
+      type: Date,
+    },
+    exitTime: {
+      type: Date,
+    },
+    placeOfVisit: {
+      type: String,
+    },
+    puproseOfVisit: {
+      type: String,
+    },
+    tag: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tag",
+      require: [true, "A transit can't be done without a tagId"],
+    },
+    vehicleNumber: String,
   },
-  exitGate: {
-    type: Number,
-  },
-  entryTime: {},
-  exitTime: {},
-  placeOfVisit: {},
-  puproseOfVisit: {},
-});
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+export const Transit = mongoose.model("Transit", transitSchema);
