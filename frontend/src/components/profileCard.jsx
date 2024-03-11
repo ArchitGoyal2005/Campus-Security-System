be2d771d385d8fe4b23b027b4350b4267ed74930
Sajal <23f2002548@ds.study.iitm.ac.in>
2024-03-11 13:46:21 +0530

    new files

---
+++ frontend/src/components/profileCard.jsx
@@ -0,0 +1,33 @@
+import { iitroorkee } from "../assets";
+
+function ProfileCard() {
+  return (
+    <section className="p-10">
+      <div className="">
+        <div className="w-96 h-[525px] rounded-[20px] shadow-3xl bg-[#CDE8E8] opacity-80">
+          <div className="flex justify-center py-8">
+            <img
+              className="rounded-full"
+              width={125}
+              height={125}
+              src={iitroorkee}
+              alt="logo"
+            />
+          </div>
+          <div className="flex justify-center font-bold text-xl">
+            <h1>Indian Institute of Technology Roorkee</h1>
+          </div>
+
+          <div className="grid grid-rows-4 text-xl p-10 leading-relaxed">
+            <p>Phone Number: 88xxxxxxxx</p>
+            <p>Roll No.: xxxxxxxxxx</p>
+            <p>Branch: Computer Science</p>
+            <p>Hostel: 4</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProfileCard;
---
+++ frontend/src/sections/profile.jsx
@@ -0,0 +1,13 @@
+// import ProfileCard from "../components/profileCard";
+import TransitRecord from "../components/transitRecord";
+
+function Profile() {
+  return (
+    <section className="bg-[url('assets/iit.svg')] bg-cover bg-center bg-no-repeat h-screen">
+      {/* <ProfileCard /> */}
+      <TransitRecord />
+    </section>
+  );
+}
+
+export default Profile;
